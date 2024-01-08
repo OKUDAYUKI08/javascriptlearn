cc57e49d87a76f5e9732432479dc9fa35a957797
OKUDAYUKI08 <yukidorakue2828@gmail.com>
2024-01-09 01:54:44 +0900

    追加で学習しました

--- 02demo/js/script.js
+++ 02demo/js/script.js
@@ -1,1 +1,4 @@
-console.log("準備完了")
+console.log("準備完了");
+const color=document.querySelector("#colorPicker");
+const text=document.querySelector("#colorText");
+text.textContent="カラーコード:"+color.value;
